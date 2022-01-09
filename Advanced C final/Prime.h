5431bc1d366c035fb8d46267d20f30d9958ac483
LAPTOP-E8OURPBO\liomi <liomilev@gmail.com>
2022-01-09 13:38:43 +0200

    doing the final in advanced C

---
+++ Advanced C final/Prime.h
@@ -0,0 +1,24 @@
+#pragma once
+
+
+
+/*
+Each time the function is called it returns the next prime number
+@returns the next prime number
+*/
+int GetNextPrime();
+
+/*
+recieves a number and checks if its a prime number or not
+@params and integer number
+@returns 1 if the number is prime
+0 if its not prime
+*/
+int IsPrime(int);
+
+
+
+
+
+
+
